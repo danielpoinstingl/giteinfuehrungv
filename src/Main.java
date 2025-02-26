db61d65ac209ed8ddd4d8524db84a52721420902
danielpoinstingl <daniel.poinstingl@gmx.at>
2025-02-26 11:44:38 +0100

    hinzufügen von hi in der main

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hi");
         System.out.println("Heute ist Mittwoch");
         System.out.println("Morgwn ist Donnerstag");
+        System.out.println("Konflikt");
     }
 }
